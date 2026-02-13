9b1f0ff192fdc10ca3499ca09db40b639b1d0551
SanthoshVarma7 <santhoshvarma2006@gmail.com>
2026-02-13 10:52:40 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,5 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to Palindrome Checker Management System\nVersion :1.0\nSystem initialized successfully.");
+    }
+}
