fb6d77a1a746a35ad55c3df2ab1cc0986476c865
SanthoshVarma7 <santhoshvarma2006@gmail.com>
2026-02-27 09:39:22 +0530

    use case 3

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Enter a string: ");
+        String originalString = scanner.nextLine();
+
+
+        String reversedString = "";
+
+
+        for (int i = originalString.length() - 1; i >= 0; i--) {
+            reversedString = reversedString + originalString.charAt(i);
+        }
+
+        if (originalString.equals(reversedString)) {
+            System.out.println("Result: The given string is a Palindrome.");
+        } else {
+            System.out.println("Result: The given string is NOT a Palindrome.");
+        }
+
+        scanner.close();
+    }
+}
